5f02e916f9231499eb5378afc930208d5876d71d
Thomas J Mather <geoipapi@maxmind.com>
2003-03-06 17:44:09 +0000

    added note about PHP < 4.3 not supporting apache_node

--- README.php
+++ README.php
@@ -1,3 +1,11 @@
+Please Note:
+apache_note is not supported in Apache2 with PHP versions < 4.3
+
+Instead use something like
+
+$country_code = $_SERVER['GEOIP_COUNTRY_CODE'];
+$country_name = $_SERVER['GEOIP_COUNTRY_NAME'];
+
 To use from PHP, use something like:
 
 <%
