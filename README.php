c96a6afa9b8511918d1f844788d106af0b8baa66
Thomas J Mather <geoipapi@maxmind.com>
2004-07-12 22:00:48 +0000

    added more PHP docs

--- README.php
+++ README.php
@@ -1,14 +1,32 @@
-Please Note:
-apache_note is not supported in Apache2 with PHP versions < 4.3
-
-Instead use something like
-
-$country_code = $_SERVER['GEOIP_COUNTRY_CODE'];
-$country_name = $_SERVER['GEOIP_COUNTRY_NAME'];
-
 To use from PHP, use something like:
 
+GeoIP Country:
 <%
 $country_code = apache_note("GEOIP_COUNTRY_CODE");
 $country_name = apache_note("GEOIP_COUNTRY_NAME");
 %>
+
+GeoIP Region:
+<%
+$country_code = apache_note("GEOIP_COUNTRY_CODE");
+$country_name = apache_note("GEOIP_REGION");
+%>
+
+GeoIP City:
+<%
+$country_code = apache_note("GEOIP_COUNTRY_CODE");
+$country_name = apache_note("GEOIP_CITY");
+%>
+
+==================================================
+Redirection with PHP
+
+$country_code = apache_note("GEOIP_COUNTRY_CODE");
+  if ( $country_code == "DE" )
+  {
+  header ("Location: http://www.google.de" );
+  }  
+  else
+  {
+  header ("Location: http://www.yoursite.com" );
+  }
