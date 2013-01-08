6bc49190f0dd5e84a575007c71a4daa30ccff7db
Boris Zentner <bzentner@maxmind.com>
2013-01-08 13:56:41 +0100

    Update version to 1.2.8

--- mod_geoip.c
+++ mod_geoip.c
@@ -49,7 +49,7 @@
 
 /* geoip module
  *
- * Version 1.2.7
+ * Version 1.2.8
  *
  * This module sets an environment variable to the remote country
  * based on the requestor's IP address.  It uses the GeoIP library
