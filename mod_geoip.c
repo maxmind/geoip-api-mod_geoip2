e0bba5004e6cb7b79f75757b0bf4e9945bebb47c
Boris Zentner <bzentner@maxmind.com>
2013-11-06 15:36:20 +0100

    Delete duplicate function call

--- mod_geoip.c
+++ mod_geoip.c
@@ -651,8 +651,6 @@ static int geoip_header_parser(request_rec * r)
 
                 set_geoip_output(cfg, r, "GEOIP_POSTAL_CODE_V6",
                                   gir->postal_code);
-                set_geoip_output(cfg, r, "GEOIP_POSTAL_CODE_V6",
-                                  gir->postal_code);
                 GeoIPRecord_delete(gir);
             }
 
